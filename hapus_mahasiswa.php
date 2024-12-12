d064d06d10334a2db5f36a9848428c32ccec5cfd
fazar301 <57576899+fazar301@users.noreply.github.com>
2024-12-12 07:10:43 +0700

    Delete hapus_mahasiswa.php

--- hapus_mahasiswa.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    include('koneksi.php');
-    $hapus = mysqli_query($db, "DELETE FROM mahasiswa WHERE nim = $_GET[nim]");
-
-    if($hapus){
-        header("location:index.php?p=mhs");
-    }
